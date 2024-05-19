9cb19c52dbae0e5380a90a65116c95f99b0826ba
RolandVyens <leiersai@sina.com>
2024-05-19 16:24:27 +0800

    sort aovs

--- __init__.py
+++ __init__.py
@@ -325,6 +325,14 @@ def sort_passes():  # 获取所有可视层输出并返回整理好的字典，�
     already_present_viewlayers = set()
     viewlayers_presented = []
     unexposed_viewlayers = []
+    material_aov = []
+    material_aovs = {}
+    for layer in bpy.context.scene.view_layers:
+        for aov in layer.aovs:
+            material_aov.append(aov.name)
+        material_aovs[layer.name] = material_aov[:]
+        material_aov.clear()
+    print(material_aovs)
     for view_layer in bpy.context.scene.view_layers:
         viewlayers.add(view_layer.name)
     for node in bpy.context.scene.node_tree.nodes:
@@ -393,6 +401,14 @@ def sort_passes():  # 获取所有可视层输出并返回整理好的字典，�
             else:
                 if "Alpha" not in i and "Denoising" not in i:
                     real_data.append(i)
+        if (
+            bpy.context.scene.IDS_AdvMode is True
+            and bpy.context.scene.IDS_UseDATALayer is True
+        ):
+            for aov in material_aovs[viewlayer]:
+                if aov in colors:
+                    colors.remove(aov)
+                    real_data.append(aov)
         viewlayer_full[viewlayer + "Data"] = real_data
         if "UV" in vector_data:
             vector_data.remove("UV")
@@ -400,14 +416,19 @@ def sort_passes():  # 获取所有可视层输出并返回整理好的字典，�
             vector_data.remove("Vector")
         viewlayer_full[viewlayer + "Vector"] = vector_data
         real_color = []
-        rgba = []
-        aov = []
         crypto = []
         for i in colors:
             if "Crypto" not in i and "Noisy" not in i and "Denoising Albedo" not in i:
                 real_color.append(i)
             if "Crypto" in i:
                 crypto.append(i)
+        if (
+            bpy.context.scene.IDS_AdvMode is True
+            and bpy.context.scene.IDS_UseDATALayer is True
+        ):
+            for aov in material_aovs[viewlayer]:
+                if aov not in real_color:
+                    real_color.append(aov)
         viewlayer_full[viewlayer + "Color"] = real_color
         viewlayer_full[viewlayer + "Crypto"] = crypto
         # print(real_data)
